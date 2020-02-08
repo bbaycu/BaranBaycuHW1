3eb972f377690935ee1e2370e12390c4779d912a
Baran Baycu <bbaycu@etu.edu.tr>
2020-02-08 14:03:46 +0300

    Created project

---
+++ src/main/java/com/baran/hw1App/App.java
@@ -0,0 +1,13 @@
+package com.baran.hw1App;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
