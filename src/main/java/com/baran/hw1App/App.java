3a019ff1dbe5f07bcf72d7a6ff31d4e968c9c347
Baran Baycu <bbaycu@etu.edu.tr>
2020-02-08 14:06:02 +0300

    Added sample function

--- src/main/java/com/baran/hw1App/App.java
+++ src/main/java/com/baran/hw1App/App.java
@@ -1,5 +1,7 @@
 package com.baran.hw1App;
 
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,15 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    public static boolean search(ArrayList<Integer> array, int e) { // TODO write a new function from scratch
+        System.out.println("inside search");
+        if (array == null) return false;
+
+        for (int elt : array) {
+            if (elt == e) return true;
+        }
+        return false;
     }
 }
